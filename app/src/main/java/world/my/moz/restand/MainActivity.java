942f961721ba76a275d2e75d44a8d60b699c6cb5
moz <moz@local>
2016-10-30 00:21:56 +0200

    set A0 working

--- app/src/main/java/world/my/moz/restand/MainActivity.java
+++ app/src/main/java/world/my/moz/restand/MainActivity.java
@@ -9,8 +9,10 @@ import android.widget.TextView;
 
 import java.io.IOException;
 
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity  {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity  {
     TextView tvTestResult;
     TextView tvScroll;
     TextView tvA0Value;
+    TextView tvA0Input;
 
     private void retrieveIds() {
         btnTest = (Button) findViewById(R.id.btnTest);
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity  {
         tvTestResult = (TextView) findViewById(R.id.tvTestResult);
         tvScroll = (TextView) findViewById(R.id.tvScroll);
         tvA0Value = (TextView) findViewById(R.id.tvA0Value);
+        tvA0Input = (TextView) findViewById(R.id.tvA0Input);
     }
 
 
@@ -60,6 +64,16 @@ public class MainActivity extends AppCompatActivity  {
                 tvA0Value.setText("(working)");
 
                 new A0GetConnect().execute(url);
+            }
+        });
+
+        btnSetA0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "http://" + tvServerInput.getText() + ":" + tvPortInput.getText() + "/gpio/A0";
+                tvA0Value.setText("(working)");
+
+                new A0PutConnect().execute(url, tvA0Input.getText().toString());
             }
         });
 
@@ -149,6 +163,48 @@ public class MainActivity extends AppCompatActivity  {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
                     .url(url)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        }
+
+    }
+
+    class A0PutConnect extends AsyncTask<String, Void, Void> {
+        String response = "";
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            tvScroll.setText( response );
+        }
+
+        @Override
+        protected Void doInBackground(String ... params ) {
+
+            try {
+                response = run( params[0], params[1] );
+            } catch (IOException e) {
+                response = null;
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        String run(String url, String value) throws IOException {
+            OkHttpClient client = new OkHttpClient();
+            GpioConverter c = new GpioConverter();
+            Gpio g = new Gpio();
+            g.setValue( value );
+            String jsonBody = c.GpioToJson( g );
+            MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+            RequestBody body = RequestBody.create(JSON, jsonBody);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .put(body)
                     .build();
 
             Response response = client.newCall(request).execute();
